051e669da98ddf229173a5290e795a4738793f7a
konser80 <konser80@gmail.com>
2021-05-30 19:14:42 +0700

    webhook

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch');
 const useful = require('useful');
-const config = require('./config.js');
+const config = require('./config');
 
 
 function notify(txt, callback) {
-  const response = fetch(config.notify_url, {
+  fetch(config.notify_url, {
     method: 'POST',
     body: JSON.stringify({ txt: useful.textify(txt, false) }),
     headers: { 'Content-Type': 'application/json' }
-  }).then(response => response.text())
-  .then(data => {
-    if (callback) callback(data)
-  });
+  })
+  .then(response => response.text())
+  .then(data => { return callback && callback(data) })
+  .catch(e => { console.error(e) });
 
   // if (callback) return callback(response);
 }
