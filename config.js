9ce7fe363e5e35c34bbef1636824035b79012acc
konser80 <konser80@gmail.com>
2021-07-06 17:46:38 +0700

    new library

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ module.exports = {
 
   notify_url: 'https://spider2.chat-monitor.ru/notifybot'
 
-}
+};
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fetch = require('node-fetch');
-const useful = require('useful');
+const tools = require('tools');
 const config = require('./config');
 
 
 function notify(txt, callback) {
   fetch(config.notify_url, {
     method: 'POST',
-    body: JSON.stringify({ txt: useful.textify(txt, false) }),
+    body: JSON.stringify({ txt: tools.textify(txt, false) }),
     headers: { 'Content-Type': 'application/json' }
   })
-  .then(response => response.text())
-  .then(data => { return callback && callback(data) })
-  .catch(e => { console.error(e) });
+    .then((response) => response.text())
+    .then((data) => {
+      if (callback) return callback(data);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
 
-  // if (callback) return callback(response);
 }
 
 
