f19a377a92fc0fe9c16443ce2471910d08939e92
konser80 <konser80@gmail.com>
2021-07-17 12:32:45 +0700

    multi message fix

--- index.js
+++ index.js
@@ -2,22 +2,50 @@ const fetch = require('node-fetch');
 const tools = require('tools');
 const config = require('./config');
 
+let job = null;
+let array = {};
 
-function notify(txt, callback) {
-  fetch(config.notify_url, {
-    method: 'POST',
-    body: JSON.stringify({ txt: tools.textify(txt, false) }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then((response) => response.text())
-    .then((data) => {
-      if (callback) return callback(data);
+function onEvent(param) {
+
+  const txt = tools.textify(param, { colors: false });
+  array[txt] = array[txt] +1 || 1;
+
+  if (job) clearTimeout(job);
+  job = setTimeout(onTimer, 1500);
+}
+
+// ==============================================
+function onTimer() {
+
+  // console.debug('[+] notify: sending array');
+  // console.debug(array);
+
+  Object.keys(array).forEach((key) => {
+    let text = key;
+    if (array[key] > 1) text += ` [count: ${array[key]}]`;
+
+    console.log(`[ ] sending text: ${text}`);
+
+    // ready? send!
+    fetch(config.notify_url, {
+      method: 'POST',
+      body: JSON.stringify({ txt: text }),
+      headers: { 'Content-Type': 'application/json' }
     })
-    .catch((e) => {
-      console.error(e);
-    });
+      .then((response) => response.text())
+      .then((data) => {
+        // if (callback) return callback(data);
+        console.debug(`[+] notify response: ${data}`);
+      })
+      .catch((e) => {
+        console.error(e.message);
+      });
 
+  });
+
+  // console.debug('[+] notify: clear array');
+  array = {};
 }
 
 
-module.exports = notify;
+module.exports = onEvent;
